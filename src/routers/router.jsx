a057d80381d2bf8c8bd8fc101fc7f90df80408d0
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-01-17 17:17:49 +0400

    aff

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App"
 import Home from "../pages/home/Home";
 import CategoryPage from "../pages/category/CategoryPage";
-import Search from "../pages/search/search";
+import Search from "../pages/search/Search";
 import ShopPage from "../pages/shop/ShopPage";
 import SingleProduct from "../pages/shop/productDetails/SingleProduct";
 import Login from "../components/Login";
